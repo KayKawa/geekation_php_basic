436d9db12d5bed099394d7964da88e49ff3b436b
KayKawa <goodenoughliving1221@gmail.com>
2022-11-15 15:53:55 +0900

    01_php_base

--- 01_php_base/lesson04.php
+++ 01_php_base/lesson04.php
@@ -7,3 +7,6 @@
 // 20歳・女性
 
 // という形で出力してください。
+$people = ['name' => '山田', 'age' => 20, 'gender' => '女性'];
+echo $people['name']."\n";
+echo $people['age']."歳 ".$people['gender'];
--- 01_php_base/lesson08.php
+++ 01_php_base/lesson08.php
@@ -1,3 +1,10 @@
 ﻿<?php
 // 引数として数値を渡すと+1して返す関数を作り、
 // 返り値を出力してください。
+function plus($x)
+{
+    return $x+1;
+}
+
+$result = plus(1);
+echo $result."\n";
